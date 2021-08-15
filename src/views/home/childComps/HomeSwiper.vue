afe36e14e4c82577e3279708a17bae111295ada2
superlin <641086053@qq.com>
2021-08-15 19:14:21 +0800

    蘑菇街商城

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+        <van-swipe-item  v-for="item in banners.list">
+          <img :src="item.image">
+        </van-swipe-item>
+    </van-swipe>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+        props:['banners'],
+        data() {
+            return {}
+        },
+        components: {},
+        methods: {}
+
+    }
+</script>
+
+<style scoped>
+    .my-swipe {
+        width: 94%;
+        margin:10px auto;
+        border-radius: 12px;
+        box-shadow:0 0 6px #eeeeee;
+        font-size:0;
+    }
+    img{
+        width: 100%;
+        height: 180px;
+        border-radius: 10px;
+    }
+</style>
